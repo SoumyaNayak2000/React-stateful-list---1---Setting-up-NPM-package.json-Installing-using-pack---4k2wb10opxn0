c1afd273d405d748d7046a32fef41d82d6670dd5
SoumyaNayak2000 <128832907+SoumyaNayak2000@users.noreply.github.com>
2023-06-22 23:28:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
+import React, { useState } from 'react'
 
-import React, { useState } from 'react';
 import '../styles/App.css';
 
 const data = {
@@ -28,34 +28,106 @@ const data = {
 }
 const App = () => {
 
-    const [selectedYear, setSelectedYear] = useState(null);
+  console.log(data);
+  const years = Object.keys(data);
+  console.log(years)
+
+  const [year, setYear] = useState('');
+  const [list, setList] = useState([]);
+
+  const selectYear = (e) => {
+    console.log('Year', e.target.value);
+    setYear(e.target.value);
+    const templist = data[e.target.value];
+    console.log(templist);
+    setList([...templist]);
+  }
+
 
   return (
     <div id="main">
+        <select onChange={selectYear}>
 
-    <select onChange={handleYearChange}>
-        <option value={null}></option>
-        {Object.keys(data).map((year) => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
-
-  <div id="selected-year">
-        {selectedYear ? `Selected year - ${selectedYear}` : 'No year selected'}
-      </div>
-        {selectedYear && (
-        <ul>
-          {data[selectedYear].map((movie) => (
-            <li key={movie}>{movie}</li>
+          <option value={null}></option>
+          {years && years.map((item, index) => (
+            <option key={index+item} value={item}>{item}</option>
           ))}
+
+        </select>
+        <br></br>
+
+        <div id="selected-year">{list.length > 0 ? 'Selected Year-' + year : 'No Year Selected'}</div>
+        <ul>
+        {
+          list && list.map((item, index) => (
+            <li key={index+item}>{item}</li>
+          ))
+        }
         </ul>
-      )}
-      
     </div>
   )
 }
 
 
 export default App;
+
+// import React, { useState } from 'react';
+// import '../styles/App.css';
+
+// const data = {
+//   '2018': [
+//     'Avengers 1',
+//     'Now you see me',
+//     'Fast and Furious 7'
+//   ],
+//   '2019': [
+//     'Avengers 2',
+//     'Now you see me 2',
+//     'Fast and Furious 8'
+//   ],
+//   '2020': [
+//     'Final Avengers Movie(Iron man dies)',
+//     'Now you finally used Lenskart',
+//     'Covid Came'
+//   ],
+//   '2021': [
+//     'Covid Returns',
+//   ],
+//   '2022': [
+//     'Adventures of Saiman',
+//     'Adventures of Shaktiman'
+//   ]
+// }
+// const App = () => {
+
+//     const [selectedYear, setSelectedYear] = useState(null);
+
+//   return (
+//     <div id="main">
+
+//     <select onChange={handleYearChange}>
+//         <option value={null}></option>
+//         {Object.keys(data).map((year) => (
+//           <option key={year} value={year}>
+//             {year}
+//           </option>
+//         ))}
+//       </select>
+
+//   <div id="selected-year">
+//         {selectedYear ? `Selected year - ${selectedYear}` : 'No year selected'}
+//       </div>
+//         {selectedYear && (
+//         <ul>
+//           {data[selectedYear].map((movie) => (
+//             <li key={movie}>{movie}</li>
+//           ))}
+//         </ul>
+//       )}
+      
+//     </div>
+//   )
+// }
+
+
+// export default App;
